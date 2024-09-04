5e2a4ca865f8a92b5ff4c42ffcf872a54b7dcef9
unknown <andreytsvl15@gmail.com>
2024-09-04 09:36:22 +0300

    seminar 1

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task1
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+// int sqr = number*number;
+// Console.WriteLine($"Квадрат числа = {sqr}");
+
+// int sqr1 = Convert.ToInt32(Math.Pow(number,3));
+// Console.WriteLine($"Куб числа = {sqr1}");
+
+// task2
+// Console.Clear();
+// Console.Write("Введите число a: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите число b: ");
+// int b = int.Parse(Console.ReadLine());
+// if (b*b==a)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+//     Console.WriteLine("нет");
+// }
+
+// task3
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// if (n==1)
+// {
+//     Console.WriteLine("понедельник");
+// }
+// else
+// {
+//     if (n==2)
+//     {
+//         Console.WriteLine("вторник");
+//     }
+//     else
+//     {
+//         if (n==3)
+//         {
+//             Console.WriteLine("среда");
+//         }
+//         else
+//         {
+//             Console.WriteLine("пора");
+//         }
+//     }
+// } 
+
+// task4
+// Console.Clear();
+// Console.Write("Введите число a: ");
+// int a = int.Parse(Console.ReadLine());
+// int i = -a;
+// while (i <= a)
+// {
+//     Console.Write(i);
+//     Console.Write(' ');
+//     i = i + 1;
+// }
+
+// task5
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+float x = a % 10;
+Console.Write(x);
+
+
+
+
+
