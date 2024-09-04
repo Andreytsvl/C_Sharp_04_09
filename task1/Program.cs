565ebcc62f1c468a4968d403f6153c0f4fee132e
unknown <andreytsvl15@gmail.com>
2024-09-04 08:09:13 +0300

    lesson 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// int value = 100;
+// Console.WriteLine(value);
+// double d=5.55;
+// char c ='!';
+// bool b = true;
+// Console.WriteLine(d);
+// Console.WriteLine(b);
+// Console.WriteLine(c);
+
+// int i1 = 5;
+// int i2 = 6;
+// int sum = i1 + i2;
+// Console.WriteLine(sum);
+
+// long i3 = 25;
+// long sum2 = i1 + i3;
+// Console.WriteLine(sum2);
+
+int n = 100;
+int i = 1;
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
